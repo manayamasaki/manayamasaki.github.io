197ff24d991bf7b5048af6395684a4294b5625f6
manayamasaki <138780699+manayamasaki@users.noreply.github.com>
2023-07-06 20:44:15 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const message = 'Hello world' // Try edit me
+
+// Update header text
+document.querySelector('#header').innerHTML = message
+
+// Log to console
+console.log(message)
+
+const arr = ["waffle", "chocolate", "mashmarrow"];
+console.log(arr)
+
+for(let item of arr){
+  console.log(item)
+}
+
+sum = 0;
+for( i=1 ; i<=100 ; ++i )
+{
+	sum = sum + i;
+}
+console.log(sum);
+
