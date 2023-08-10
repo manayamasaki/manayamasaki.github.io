1a26552dc3768e01ad8fd3060828e367eebb115e
manayamasaki <138780699+manayamasaki@users.noreply.github.com>
2023-08-10 18:07:45 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,69 @@
-const message = 'Hello world' // Try edit me
+'use strict'
+// Please don't delete the 'use strict' line above
 
-// Update header text
-document.querySelector('#header').innerHTML = message
+const school="Waffle College";
+console.log(school);//
 
-// Log to console
-console.log(message)
+const name="mana";
+console.log(name);
 
-const arr = ["waffle", "chocolate", "mashmarrow"];
-console.log(arr)
+let age=18;
+console.log(age);
 
-for(let item of arr){
-  console.log(item)
-}
+const isProgrammer ="true";
+console.log(isProgrammer);
 
-sum = 0;
-for( i=1 ; i<=100 ; ++i )
-{
-	sum = sum + i;
-}
+let currentTask = 1;
+console.log(currentTask);
+currentTask++;
+console.log(currentTask);
+
+
+const squareSideLength=2;
+const squareBottomLength=2;
+const squareArea = squareSideLength * squareBottomLength;
+console.log(squareArea);
+
+const rectangleBaseLength=3;
+const rectangleHeightLength=4;
+rectangle=rectangleBaseLength*rectangleHeightLength
+console.log(rectangle);
+
+const triangleBaseLength=4;
+const triangleHeightLength=5;
+triangleArea=triangleBaseLength*triangleHeightLength/2;
+console.log(triangleArea);
+
+const variableNumber = 1;
+console.log(typeof variableNumber);
+
+const variableBoolean=true;
+console.log(typeof variableBoolean);
+
+const variableUndefined=;
+console.log(typeof variableUndefined);
+
+const numberA = 10;
+const numberB = 20;
+const average = (numberA+numberB)/2;
+console.log(average);
+
+const circleDiameter = 10;
+circleCircumferece=circleDiameter*3.14;
+circleArea=(circleDiameter/2)*(circleDiameter/2)*3.14;
+console.log(circleCircumferece);
+console.log(circleArea);
+
+let a = "B";
+let b = "A";
+a = "A";
+b = "B";
+console.log(a);
+console.log(b);
+
+//合計金額
+const onion =80;
+const tomato =100;
+sum=onion+tomato;
 console.log(sum);
 
